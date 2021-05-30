9e22ee9d9e51df680613caac2accb3d09fcdcc2f
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-05-30 13:14:32 +0200

    Prismic fetching initial setup

---
+++ src/lib/constants/prismicClient.ts
@@ -0,0 +1,3 @@
+import Prismic from '@prismicio/client';
+
+export const prismicClient = Prismic.client('https://pawel-blaszczyk.cdn.prismic.io/api/v2');
---
+++ src/routes/blog/[uid].svelte
@@ -0,0 +1,31 @@
+<script lang="ts" context="module">
+	import { prismicClient } from '$lib/constants/prismicClient';
+
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit/types/page';
+	import type { Document } from '@prismicio/client/types/documents';
+
+	export const load = async (input: LoadInput): Promise<LoadOutput> => {
+		const post = await prismicClient.getByUID('blog', input.page.params.uid, {});
+
+		if (post) {
+			return {
+				props: {
+					post
+				}
+			};
+		}
+
+		return {
+			status: 404,
+			error: new Error(`Could not load blog post ${input.page.params.uid}`)
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let post: Document;
+</script>
+
+<div>
+	{JSON.stringify(post)}
+</div>
