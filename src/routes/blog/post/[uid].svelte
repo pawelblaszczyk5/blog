d596e6b62a1ed3c2540f1e56ac7cf8ad8edf68e2
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-13 13:35:23 +0200

    Post fetching adjustments

--- src/routes/blog/post/[uid].svelte
+++ src/routes/blog/post/[uid].svelte
@@ -5,20 +5,27 @@
 	import type { Document } from '@prismicio/client/types/documents';
 
 	export const load = async (input: LoadInput): Promise<LoadOutput> => {
-		const post = await prismicClient.getByUID('blog', input.page.params.uid, {});
+		try {
+			const post = await prismicClient.getByUID('blog', input.page.params.uid, {});
 
-		if (post) {
+			if (post) {
+				return {
+					props: {
+						post
+					}
+				};
+			}
+
 			return {
-				props: {
-					post
-				}
+				status: 404,
+				error: new Error(`Could not load blog post by id ${input.page.params.uid}`)
+			};
+		} catch (e) {
+			return {
+				status: 500,
+				error: new Error('Error during post fetching')
 			};
 		}
-
-		return {
-			status: 404,
-			error: new Error(`Could not load blog post by id ${input.page.params.uid}`)
-		};
 	};
 </script>
 
