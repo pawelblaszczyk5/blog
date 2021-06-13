a54559c100b3df197eb621e47a78d82d622d9980
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-13 13:54:57 +0200

    Added fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource/playfair-display/variable.css';
+	import '@fontsource/source-sans-pro/400.css';
+	import '@fontsource/source-sans-pro/600.css';
 </script>
 
 <main>
@@ -7,6 +10,16 @@
 </main>
 
 <style>
+	:root {
+		--serifFont: 'Playfair DisplayVariable', serif;
+		--sansSerifFont: 'Source Sans Pro', sans-serif;
+		font-size: 16px;
+	}
+
+	:global(body) {
+		font-family: var(--serifFont);
+	}
+
 	main {
 		max-width: 1200px;
 		margin: 0 auto;
