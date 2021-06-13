360bf8cd03ce6eb3ba4f6d25ed93724d0530cc19
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-13 14:15:11 +0200

    Theme colors WIP, dark mode added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,20 @@
 	import '@fontsource/playfair-display/variable.css';
 	import '@fontsource/source-sans-pro/400.css';
 	import '@fontsource/source-sans-pro/600.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const darkMode = localStorage.getItem('darkMode');
+		if (darkMode === 'on') {
+			document.body.classList.add('dark');
+			return;
+		} else if (!darkMode) {
+			if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+				document.body.classList.add('dark');
+				localStorage.setItem('darkMode', 'on');
+			}
+		}
+	});
 </script>
 
 <main>
@@ -11,13 +25,27 @@
 
 <style>
 	:root {
-		--serifFont: 'Playfair DisplayVariable', serif;
-		--sansSerifFont: 'Source Sans Pro', sans-serif;
 		font-size: 16px;
 	}
 
 	:global(body) {
+		--serifFont: 'Playfair DisplayVariable', serif;
+		--sansSerifFont: 'Source Sans Pro', sans-serif;
+		--backgroundColor: #fdfffc;
+		--fontColor: #011627;
+		--primaryAccentColor: #2ec4b6;
+		--secondaryAccentColor: #e71d36;
+
 		font-family: var(--serifFont);
+		background-color: var(--backgroundColor);
+		color: var(--fontColor);
+	}
+
+	:global(body.dark) {
+		--backgroundColor: #0a1128;
+		--fontColor: #fefcfb;
+		--primaryAccentColor: #034078;
+		--secondaryAccentColor: #1282a2;
 	}
 
 	main {
