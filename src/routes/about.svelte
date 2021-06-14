b41fe5f1a8a6c2ffb848ba1702e147c327614f21
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-14 19:15:46 +0200

    About page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,3 +3,45 @@
 </svelte:head>
 
 <h1>About</h1>
+<p>You can always find and contact me in the below places:</p>
+<ul>
+	<li>
+		<a target="_blank" rel="external" href="https://github.com/pawelblaszczyk5/" class="link"
+			>Github</a
+		>
+	</li>
+	<li>
+		<a
+			target="_blank"
+			rel="external"
+			href="https://www.linkedin.com/in/pawel-blaszczyk/"
+			class="link">LinkedIn</a
+		>
+	</li>
+	<li>
+		<a target="_blank" rel="external" href="mailto:pawelblaszczyk@wir.pl" class="link">Email</a>
+	</li>
+</ul>
+<p>
+	The logo of this site has been made by my amazing girlfriend - you can find more of her works <a
+		target="_blank"
+		href="https://www.instagram.com/ada_guzik/"
+		rel="external"
+		class="link">here</a
+	>.
+</p>
+
+<style>
+	ul {
+		list-style-position: inside;
+	}
+
+	a {
+		margin: 0.5rem 0;
+		display: inline-block;
+	}
+
+	a {
+		font-family: var(--serifFont);
+	}
+</style>
